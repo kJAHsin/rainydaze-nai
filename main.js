acb3f5db76286a4030a391dc618a30d2da31541f
kJAHsin <kjahnsencollins@gmail.com>
2023-04-24 21:12:29 +0200

    first day setup nav and carousel

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const baseURL = "https://api.noroff.dev/api/v1/";
+const apiURL = "https://api.noroff.dev/api/v1/rainy-days";
+fetch(apiURL)
+	.then(function (data) {
+        console.log(data.json());
+		return data.json();
+	})
