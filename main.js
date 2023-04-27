71a83b9bc5da230bad47b1432194bbfa19e1662f
kJAHsin <kjahnsencollins@gmail.com>
2023-04-27 10:05:54 +0200

    added data console log of api

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const apiURL = "https://api.noroff.dev/api/v1/rainy-days";
 fetch(apiURL)
-	.then(function (data) {
-        console.log(data.json());
-		return data.json();
-	})
+	.then(res => res.json())
+	.then(data => console.log(data))
