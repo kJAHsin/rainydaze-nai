77c36aa2943f58857da83330dff712d3cc8fc59b
kJAHsin <kjahnsencollins@gmail.com>
2023-04-30 01:20:56 +0200

    done for the day

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ const scrollArrowR = document.querySelector(".scroll__arrow:nth-of-type(2)");
 scrollArrowL.addEventListener("click", () => {
 	console.log("fuck you")
 })
+
+scrollArrowR.addEventListener("click", () => {
+	console.log("fuck you")
+})
+
+// grabbing nav menu toggle
+const burgerClosed = document.getElementById("burgerClosed");
+
+// grabbing svg lines in toggle and adding transition
+const burgerTop = document.getElementById("topLine");
+const burgerMid = document.getElementById("midLine");
+const burgerBot = document.getElementById("botLine");
+burgerTop.style.transition = "all 3s ease-out";
+burgerMid.style.transition = "all 3s ease-out";
+burgerBot.style.transition = "all 3s ease-out";
+burgerTop.style.transformOrigin = "bottom";
+burgerBot.style.transformOrigin = "top";
+
+// change attributes on click
+const navToggle = document.querySelector(".hamburger__link");
+
+navToggle.addEventListener("click", () => {
+	if (burgerClosed.ariaExpanded === "false") {
+		burgerTop.style.transform = "translateY(25%)";
+		burgerMid.style.opacity = 0;
+		burgerBot.style.transform = "translateY(-25%)";
+		burgerClosed.ariaExpanded = "true";
+		burgerTop.style.transform = "translate(-51%, 2.75%) rotate(45deg)";
+		burgerBot.style.transform = "translate(-51%, -2.75%) rotate(-45deg)";
+	}
+	else {
+		console.log("ouch");
+		burgerTop.style.transform = "translateY(0)";
+		burgerMid.style.opacity = 1;
+		burgerBot.style.transform = "translateY(0)";
+		burgerClosed.ariaExpanded = "false";
+	}
+	
+})
--- products.js
+++ products.js
@@ -3,20 +3,20 @@ const carouselCards = document.querySelectorAll(".carousel__card");
 
 const fetchProducts = () => {
 	fetch(apiURL)
-		.catch((err) => console.log(`you got an error bro: ${err}`))
-		.then((data) => data.json())
-		.then((data) => data.forEach((product, idx) => {
-			const img = document.createElement("img");
-			const id = `${product.id}`;
-			const url = `${product.image}`;
-			const alt = `${product.description}`;
-			const salePrice = `${product.discountedPrice}`;
-			img.dataset.prodID = id;
-			img.src = url;
-			img.alt = alt;
-			img.dataset.salePrice = salePrice;
-			carouselCards[idx].append(img);
-		}));
+	.then((data) => data.json())
+	.then((data) => data.forEach((product, idx) => {
+		const img = document.createElement("img");
+		const id = `${product.id}`;
+		const url = `${product.image}`;
+		const alt = `${product.description}`;
+		const salePrice = `${product.discountedPrice}`;
+		img.dataset.prodID = id;
+		img.src = url;
+		img.alt = alt;
+		img.dataset.salePrice = salePrice;
+		carouselCards[idx].append(img);
+	}))
+	.catch((err) => console.log(`you got an error bro: ${err}`))
 };
 
 export default fetchProducts;
