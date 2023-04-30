1673c6a7df5c3e31a5276068ca6a8d769e3d8350
kJAHsin <kjahnsencollins@gmail.com>
2023-04-30 12:44:23 +0200

    fixed toggle animation time and put into variables

--- main.js
+++ main.js
@@ -5,18 +5,19 @@ const scrollArrowL = document.querySelector(".scroll__arrow:first-of-type");
 const scrollArrowR = document.querySelector(".scroll__arrow:nth-of-type(2)");
 
 scrollArrowL.addEventListener("click", () => {
-	console.log("fuck you")
+	console.log("to the left to the left")
 })
 
 scrollArrowR.addEventListener("click", () => {
-	console.log("fuck you")
+	console.log("right step now ya'll")
 })
 
 // grabbing nav menu toggle
 const burgerClosed = document.getElementById("burgerClosed");
 
 // grabbing svg lines in toggle and adding transition
-const transition = "all 1s ease-out";
+const transitionTime = "450";
+const transition = `all ${transitionTime}ms ease-out`;
 
 const burgerTop = document.getElementById("topLine");
 const burgerMid = document.getElementById("midLine");
@@ -32,6 +33,7 @@ const navToggle = document.querySelector(".hamburger__link");
 
 navToggle.addEventListener("click", () => {
 	if (burgerClosed.ariaExpanded === "false") {
+		console.log("open nav toggle")
 		burgerTop.style.transform = "translateY(25%)";
 		burgerMid.style.opacity = 0;
 		burgerBot.style.transform = "translateY(-25%)";
@@ -40,10 +42,10 @@ navToggle.addEventListener("click", () => {
 		setTimeout(function twist() {
 			burgerTop.style.transform = "translate(-51%, 2.75%) rotate(45deg)";
 			burgerBot.style.transform = "translate(-51%, -2.75%) rotate(-45deg)";
-		}, 1000)
+		}, transitionTime)
 	}
 	else {
-		console.log("ouch");
+		console.log("close nav toggle");
 		burgerTop.style.transform = "translateY(0)";
 		burgerMid.style.opacity = 1;
 		burgerBot.style.transform = "translateY(0)";
