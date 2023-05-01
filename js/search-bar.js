bddcbd59d7bbaa4f94345058f79ef171de7516a9
kJAHsin <kjahnsencollins@gmail.com>
2023-05-01 18:10:53 +0200

    dynamic search on products page working

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,11 +1,16 @@
 // filter through products with search
 export function search() {
 	const searchBar = document.getElementById("jacketSearch");
-    const productContainer = document.querySelector(".products__container");
-	const products = document.querySelectorAll(".product")
-    console.log(productContainer);
+	const products = document.querySelectorAll(".product");
 	searchBar.addEventListener("keyup", () => {
-		let searchQuery = searchBar.value;
-        console.log(searchQuery)
+		let searchQuery = searchBar.value.toLowerCase();
+		console.log(searchQuery)
+		products.forEach((product) => {
+			product.classList.add("hidden");
+			const title = product.dataset.title.toLowerCase();
+			if (title.includes(searchQuery)) {
+				product.classList.remove("hidden");
+			}			
+		});
 	});
 }
