d70ccea9a8e0980344411232196a69c54bc13b4a
kJAHsin <kjahnsencollins@gmail.com>
2023-05-01 22:40:59 +0200

    mastered the search filter

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,33 +1,48 @@
+// function resets search query suggestions
+function clearQuery() {
+	const currentOptions = searchOptions.querySelectorAll("li");
+		currentOptions.forEach(option => {
+			option.remove();
+		})
+}
+
+
+
+
+
+
 // filter through products with search
 export function search() {
 	const searchBar = document.getElementById("jacketSearch");
 	const products = document.querySelectorAll(".product");
 	const searchOptions = document.getElementById("searchOptions");
 
+	// keyup listener for search bar
+	// updates search query suggestions on keyup
+	// updates product list on "enter"
+	// (e) for use in if loop to see if it is the enter key
 	searchBar.addEventListener("keyup", (e) => {
 		let searchQuery = searchBar.value.toLowerCase();
-		const currentOptions = searchOptions.querySelectorAll("li");
-		currentOptions.forEach(option => {
-			option.remove();
-		})
+		clearQuery();		
 		products.forEach((product) => {
 			const productIMG = product.querySelector("img");
 			const title = product.dataset.title.toLowerCase();
 
+			// updating product list on enter
 			if (e.key === "Enter") {
 				product.classList.add("hidden");
+				// clearing query suggesstions after search
+				clearQuery();
 				if (title.includes(searchQuery)) {
 					// showing item if present in search query
 					product.classList.remove("hidden");
 				}
-			}
+			}		
+
 			
-
-
-			if (title.includes(searchQuery)) {
-				
-
-				// filtering search options from input text
+			// filtering search options from input text
+			if (title.includes(searchQuery) & searchQuery.length > 0) {	
+				// creating search query option dropdown
 				const searchOption = document.createElement("li");
 				searchOption.innerHTML = `<div class="thumbnail">
 				<img src="${productIMG.src}" alt="${productIMG.alt}">
@@ -40,12 +55,3 @@ export function search() {
 	});
 }
 
-function clearResults() {
-    let currentImages = gallery.querySelectorAll('img');
-
-    currentImages.forEach(currentImage => {
-        if (currentImage) {
-            currentImage.remove();
-        }
-    })
-}
