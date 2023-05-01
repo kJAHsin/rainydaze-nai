d0f99fef73a8edc40d2b32fa16f131a1a256bb51
kJAHsin <kjahnsencollins@gmail.com>
2023-05-01 12:45:53 +0200

    finished animation on nav toggle and added products.html

--- navToggle.js
+++ navToggle.js
@@ -16,26 +16,16 @@ export const toggleNav = () => {
 	navToggle.addEventListener("click", () => {
 		if (burgerClosed.ariaExpanded === "false") {
 			console.log("open nav toggle");
-			// burgerTop.style.transform = "translateY(25%)";
-			// burgerMid.style.opacity = 0;
-			// burgerBot.style.transform = "translateY(-25%)";
             // set ariaExpanded to true
 			burgerClosed.ariaExpanded = "true";
             burgerTop.classList.add("navOpenTop");
             burgerMid.classList.add("navOpenMid");
-            burgerTop.classList.add("navOpenBot");
-            // delayed twist animation
-			// setTimeout(function twist() {
-			// 	burgerTop.style.transform =
-			// 		"translate(-51%, 2.75%) rotate(45deg)";
-			// 	burgerBot.style.transform =
-			// 		"translate(-51%, -2.75%) rotate(-45deg)";
-			// }, transitionTime);
+            burgerBot.classList.add("navOpenBot");
 		} else {
 			console.log("close nav toggle");
-			burgerTop.style.transform = "translateY(0)";
-			burgerMid.style.opacity = 1;
-			burgerBot.style.transform = "translateY(0)";
+			burgerTop.classList.remove("navOpenTop");
+            burgerMid.classList.remove("navOpenMid");
+            burgerBot.classList.remove("navOpenBot");
             // set ariaExpanded to false
 			burgerClosed.ariaExpanded = "false";
 		}
